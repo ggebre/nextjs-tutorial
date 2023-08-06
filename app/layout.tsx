f9b1b8c6e09dc0e3071d63053fd07319b8b022fb
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-05 23:44:43 -0500

    update parent layout and add layout to about route

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <nav> <h1> MY nav bar</h1></nav>
+        {children}</body>
     </html>
   )
 }
