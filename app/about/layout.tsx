c5c81903e4f697f534ad63fa15b03aeb72f6972f
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-05 23:57:46 -0500

    update styles for pages

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,3 +1,4 @@
+import styles from './styles.module.css'
 export default function AboutLayout({
     children,
   }: {
@@ -6,7 +7,7 @@ export default function AboutLayout({
     return (
       <>
         <nav>About Navbar</nav>
-        <main>
+        <main className={styles.main}>
             {children}
         </main>
       </>
