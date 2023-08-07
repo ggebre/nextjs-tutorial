d3edad88834658767ffc1d67363645cfd041d5f7
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-06 22:17:30 -0500

    create UserPosts component to render user posts data

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,5 +1,7 @@
 import getUser from "@/lib/getUser";
 import getUserPost from "@/lib/getUserPosts";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
 type Params = {
     params: {
         userId: string,
@@ -9,13 +11,17 @@ export default async function User( {params: { userId } }: Params ) {
     const userData: Promise<User> = getUser(userId);
     const userPostsData: Promise<Post[]> = getUserPost(userId);
 
-    const [user, userPosts ] = await Promise.all([userData, userPostsData]);
+    // const [user, userPosts ] = await Promise.all([userData, userPostsData]);
+    const user = await userData
 
     return (
         <>
             <h2> {user.name}</h2>
             <br />
-            {/* <UserPosts posts={userPosts} /> */}
+            {/* waits on the data arrival of posts data */}
+            <Suspense fallback={<h2>Loading Posts...</h2>}>
+                <UserPosts promise={userPostsData} />
+            </Suspense>
             
         </>
     )
