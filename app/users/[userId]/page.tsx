1b16ef0f8cb894de64420a3f850c81c5cbb545a4
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-07 14:09:20 -0500

    create dynamic metadata for userpage

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -2,12 +2,24 @@ import getUser from "@/lib/getUser";
 import getUserPost from "@/lib/getUserPosts";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
+import type { Metadata} from 'next';
+
 type Params = {
     params: {
         userId: string,
     }
 }
-export default async function User( {params: { userId } }: Params ) {
+// generate dynamic metadata for each page
+export async function generateMetadata({params: { userId } }: Params): Promise<Metadata> {
+    const userData : Promise<User> = getUser(userId);
+    const user: User = await userData
+
+    return {
+        title: user.name,
+        description: `This is the page of ${user.name}`
+    }
+}
+export default async function UserPage( {params: { userId } }: Params ) {
     const userData: Promise<User> = getUser(userId);
     const userPostsData: Promise<Post[]> = getUserPost(userId);
 
@@ -18,7 +30,7 @@ export default async function User( {params: { userId } }: Params ) {
         <>
             <h2> {user.name}</h2>
             <br />
-            {/* waits on the data arrival of posts data */}
+            {/* waits on the data arrival of posts data for a given user*/}
             <Suspense fallback={<h2>Loading Posts...</h2>}>
                 <UserPosts promise={userPostsData} />
             </Suspense>
