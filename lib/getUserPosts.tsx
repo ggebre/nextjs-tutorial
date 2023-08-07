11f189b4c2c1c47a3cc3049301ea98914e323e24
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-07 14:24:15 -0500

    add revalidation on requested data

--- lib/getUserPosts.tsx
+++ lib/getUserPosts.tsx
@@ -1,6 +1,7 @@
 export default async function getUserPosts(userId: string) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-    
+    const res = await fetch(`https://jsonplaceholder.typicode.com/
+    posts?userId=${userId}`, { next: { revalidate: 60}})
+    // revalidate checks if there is update on data in 60 seconds 
     if(!res.ok) throw new Error('Failed to fetch data')
 
     return res.json()
