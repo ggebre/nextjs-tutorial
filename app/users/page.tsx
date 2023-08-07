647cb997733de2f5039d6f8fc610463539904c0f
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-06 21:27:17 -0500

    define type for user object

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import type { Metadata } from 'next';
+import getAllUsers from '@/lib/getuAllUsers';
 
 export const metadata: Metadata = {
     title : 'Users',
 }
 
-export default function UsersPage ()  {
+export default async function UsersPage ()  {
+    // const usersData: Promise<> = getAllUsers()
     return (
         <div>Users</div>
     )
